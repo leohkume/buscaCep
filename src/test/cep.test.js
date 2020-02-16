3a93856be4df26bdc8d819e1b7ffa855d7b29841
Leonardo Kume <leonardo.kume@vitta.me>
2020-02-16 17:17:20 -0300

    feat: unit tests

--- src/test/cep.test.js
+++ src/test/cep.test.js
@@ -1,6 +1,11 @@
 const Correios = require('../models/correios');
 
 test('find cep', async done => {
-    await new Correios().findCep({ "cep": "02074000" })
+    let cep = await new Correios().findCep({ "cep": "02074000" })
+    expect(typeof cep === "object").toBe(true);
+    expect(cep.cep == "02074000").toBe(true);
+    expect(cep.end.trim()).toEqual("Rua do Imperador - até 639/640");
+    expect(cep.bairro).toBeDefined();
+    expect(cep.local.trim() === "São Paulo/SP").toBe(true);
     done()
 });
